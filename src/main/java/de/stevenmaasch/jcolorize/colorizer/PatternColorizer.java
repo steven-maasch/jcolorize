16b5d8d8aa1129441adaa09322e4d6c59c0a6876
Steven Maasch <steven.maasch@gmx.de>
2015-07-22 12:25:14 +0200

    Bracket bug

--- src/main/java/de/stevenmaasch/jcolorize/colorizer/PatternColorizer.java
+++ src/main/java/de/stevenmaasch/jcolorize/colorizer/PatternColorizer.java
@@ -8,7 +8,7 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 import de.stevenmaasch.jcolorize.util.AnsiEscape;
-import de.stevenmaasch.jcolorize.util.RegexPattern;
+import de.stevenmaasch.jcolorize.util.MatchPattern;
 
 public final class PatternColorizer {
 
@@ -35,16 +35,20 @@ public final class PatternColorizer {
 	
 	public static PatternColorizer getInstance() {
 		final ArrayList<Pair<Pattern, AnsiEscape>> mappings = new ArrayList<Pair<Pattern, AnsiEscape>>();
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.BRACKETS, AnsiEscape.FG_CYAN));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.MAC_ADDR, AnsiEscape.FG_CYAN));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.IP4_ADDR_WITH_PORT, AnsiEscape.FG_MAGENTA));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.HTTP_METHOD, AnsiEscape.FG_CYAN));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.DATE_ISO8601, AnsiEscape.FG_BLUE));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.TIME_ISO8601, AnsiEscape.FG_BLUE));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.LOGLEVEL_ERROR, AnsiEscape.FG_RED));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.LOGLEVEL_FATAL, AnsiEscape.FG_RED));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.LOGLEVEL_WARNING, AnsiEscape.FG_YELLOW));
-		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(RegexPattern.LOGLEVEL_DEBUG, AnsiEscape.FG_CYAN));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_WORDS_GOOD, AnsiEscape.FG_GREEN));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_WORDS_BAD, AnsiEscape.FG_YELLOW));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_WORDS_ERROR, AnsiEscape.FG_RED));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_MAC_ADDR, AnsiEscape.FG_CYAN));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_IP4_ADDR_WITH_PORT, AnsiEscape.FG_MAGENTA));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_HTTP_METHOD, AnsiEscape.FG_BLUE));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_DATE_ISO8601, AnsiEscape.FG_BLUE));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_TIME_ISO8601, AnsiEscape.FG_BLUE));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_LOGLEVEL_ERROR, AnsiEscape.FG_RED));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_LOGLEVEL_FATAL, AnsiEscape.FG_RED));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_LOGLEVEL_WARNING, AnsiEscape.FG_YELLOW));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_LOGLEVEL_DEBUG, AnsiEscape.FG_CYAN));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_SEQUENCE_OF_SAME_CHARACTERS, AnsiEscape.FG_CYAN));
+		mappings.add(new ImmutablePair<Pattern, AnsiEscape>(MatchPattern.RE_BRACKETS, AnsiEscape.FG_CYAN));
 		return new PatternColorizer(mappings);
 	}
 	
---
+++ src/main/java/de/stevenmaasch/jcolorize/util/MatchPattern.java
@@ -0,0 +1,83 @@
+package de.stevenmaasch.jcolorize.util;
+
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+
+public final class MatchPattern {
+
+	private static final String[] WORDS_GOOD = {
+			"started", "activ", "ready", "online",
+			"configured", "enable", "open",
+			"complete", "done", "connected",
+			"finish", "clean"
+	};
+
+	private static final String[] WORDS_BAD = {
+			"warn", "restart", "exit", "stop", "shutting",
+			"down", "close", "unreach", "can't", "cannot", "skip",
+			"deny", "disable", "ignored", "missing", "blocking",
+			"ignore", "unable", "offline", "terminat", "empty",
+			"null", "exception", "throw"
+	};
+
+	private static final String[] WORDS_ERROR = {
+			"error", "critical", "invalid", "fail", "alarm", "fatal"	
+	};
+	
+	private static final String RE_WORD_BOUNDARY = "\\b";
+	
+	private static final Pattern RE_NO_ESACPE_LOOKBEHIND = Pattern.compile("(?<!\\u001b)");
+	
+	private static final String WORDS_FORMAT = "(\\w*(%s)\\w*)";
+	
+	public static final Pattern RE_WORDS_GOOD =
+			Pattern.compile(join(WORDS_GOOD, "|", WORDS_FORMAT), Pattern.CASE_INSENSITIVE);
+	
+	public static final Pattern RE_WORDS_BAD =
+			Pattern.compile(join(WORDS_BAD, "|", WORDS_FORMAT), Pattern.CASE_INSENSITIVE);
+	
+	public static final Pattern RE_WORDS_ERROR =
+			Pattern.compile(join(WORDS_ERROR, "|", WORDS_FORMAT), Pattern.CASE_INSENSITIVE);
+
+	public static final Pattern RE_BRACKETS = Pattern.compile(RE_NO_ESACPE_LOOKBEHIND.pattern() + "[\\(\\)\\[\\]]");
+
+	public static final Pattern RE_MAC_ADDR =
+			Pattern.compile(RE_WORD_BOUNDARY + "([0-9A-F]{2}[:-]){5}([0-9A-F]{2})" + RE_WORD_BOUNDARY);
+
+	public static final Pattern RE_IP4_ADDR_WITH_PORT =
+			Pattern.compile(RE_WORD_BOUNDARY + "(\\d{1,3}\\.){3}\\d{1,3}(:\\d+)?" + RE_WORD_BOUNDARY);
+
+	public static final Pattern RE_DATE_ISO8601 =
+			Pattern.compile(RE_WORD_BOUNDARY + "\\d{4}-\\d{2}-\\d{2}" + RE_WORD_BOUNDARY);
+
+	public static final Pattern RE_TIME_ISO8601 =
+			Pattern.compile(RE_WORD_BOUNDARY + "\\d{2}:\\d{2}:\\d{2}(,\\d{1,3})?" + RE_WORD_BOUNDARY);
+
+	public static final Pattern RE_LOGLEVEL_DEBUG =
+			Pattern.compile(RE_WORD_BOUNDARY + "DEBUG" + RE_WORD_BOUNDARY);
+
+	public static final Pattern RE_LOGLEVEL_WARNING =
+			Pattern.compile(RE_WORD_BOUNDARY + "WARN(ING)?" + RE_WORD_BOUNDARY);
+
+	public static final Pattern RE_LOGLEVEL_ERROR =
+			Pattern.compile(RE_WORD_BOUNDARY + "ERROR" + RE_WORD_BOUNDARY);
+
+	public static final Pattern RE_LOGLEVEL_FATAL =
+			Pattern.compile(RE_WORD_BOUNDARY+ "FATAL" + RE_WORD_BOUNDARY);
+
+	public static final Pattern RE_HTTP_METHOD =
+			Pattern.compile(RE_WORD_BOUNDARY + "GET|POST|HEAD|PUT|DELETE|TRACE|OPTIONS|CONNECT" + RE_WORD_BOUNDARY);
+
+	/**
+	 * Matches a sequence of more than <u>4</u> same characters
+	 */
+	public static final Pattern RE_SEQUENCE_OF_SAME_CHARACTERS = Pattern.compile("(.)\\1{4,}");
+
+	private MatchPattern() { }
+	
+	private static String join(String[] array, String seperator, String format) {
+		return String.format(format, StringUtils.join(array, seperator));
+	}
+
+}
